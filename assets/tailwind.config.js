59e153da4d31fa80401c90341e5e9004080ea29a
Steven Nunez <octosteve@github.com>
2021-10-21 11:43:14 -0400

    Add TailwindCSS

---
+++ assets/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./js/**/*.js", "../lib/*_web/**/*.*ex"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {},
+  variants: {},
+  plugins: [],
+};
--- config/dev.exs
+++ config/dev.exs
@@ -25,7 +25,15 @@ config :agartala, AgartalaWeb.Endpoint,
   secret_key_base: "Hrx2mN8fJEcfPs+5VJ8Y82lAqKBit0KtF4alWvbvpiHEZHUfp/IFOns2mFLQ9EVi",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    npx: [
+      "tailwindcss",
+      "--input=css/app.css",
+      "--output=../priv/static/assets/app.css",
+      "--postcss",
+      "--watch",
+      cd: Path.expand("../assets", __DIR__)
+    ]
   ]
 
 # ## SSL Support
